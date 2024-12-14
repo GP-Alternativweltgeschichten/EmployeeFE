edb015aa97ed7dece7d833dabb25986acd9d9e9c
Hanna Sophie Müller <mhannasophie@gmail.com>
2024-12-14 13:41:43 +0100

    changed file structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {PromptingComponent} from './prompting/prompting.component';
-import {ScenariosComponent} from './scenarios/scenarios.component';
-import {OldMapsComponent} from './old-maps/old-maps.component';
+import {PromptingComponent} from './visitor/prompting/prompting.component';
+import {ScenariosComponent} from './visitor/scenarios/scenarios.component';
+import {OldMapsComponent} from './visitor/old-maps/old-maps.component';
 import {HomeComponent} from './home/home.component';
-import {AdminScenariosComponent} from './admin-scenarios/admin-scenarios.component';
-import {AdminOldMapsComponent} from './admin-old-maps/admin-old-maps.component';
+import {AdminScenariosComponent} from './admin/admin-scenarios/admin-scenarios.component';
+import {AdminOldMapsComponent} from './admin/admin-old-maps/admin-old-maps.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
-import { NavBarVisitorComponent } from './nav-bar-visitor/nav-bar-visitor.component';
-import { PromptingComponent } from './prompting/prompting.component';
-import { OldMapsComponent } from './old-maps/old-maps.component';
-import { ScenariosComponent } from './scenarios/scenarios.component';
+import { NavBarVisitorComponent } from './visitor/nav-bar-visitor/nav-bar-visitor.component';
+import { PromptingComponent } from './visitor/prompting/prompting.component';
+import { OldMapsComponent } from './visitor/old-maps/old-maps.component';
+import { ScenariosComponent } from './visitor/scenarios/scenarios.component';
 import {MenubarModule} from 'primeng/menubar';
 import { FooterComponent } from './footer/footer.component';
 import {Button} from 'primeng/button';
@@ -20,10 +20,10 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import {ToggleButtonModule} from "primeng/togglebutton";
 import {FormsModule} from '@angular/forms';
 import { HomeComponent } from './home/home.component';
-import { NavBarAdminComponent } from './nav-bar-admin/nav-bar-admin.component';
+import { NavBarAdminComponent } from './admin/nav-bar-admin/nav-bar-admin.component';
 import {SelectButtonModule} from 'primeng/selectbutton';
-import { AdminScenariosComponent } from './admin-scenarios/admin-scenarios.component';
-import { AdminOldMapsComponent } from './admin-old-maps/admin-old-maps.component';
+import { AdminScenariosComponent } from './admin/admin-scenarios/admin-scenarios.component';
+import { AdminOldMapsComponent } from './admin/admin-old-maps/admin-old-maps.component';
 
 // Factory function for ngx-translate
 export function HttpLoaderFactory(http: HttpClient) {
